f77b2409033df6a25fa77f5f7d0eb60cc823c4be
Krishnagupta5108 <kgupta7173@gmail.com>
2026-08-18 10:26:32 +0530

    chnage 4

---
+++ Bunch3/B3PractCode6.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+ 
+int main (){
+    int a , b  ; 
+    cout<<"Enter a and  b ";
+    cin>>a>>b ; 
+    int gcd  ; 
+    for (int  i = 1; i <= a/2; i++)
+    {
+        gcd = a / b ; 
+        
+    }
+    
+ 
+ 
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
